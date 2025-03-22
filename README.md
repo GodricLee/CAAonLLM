efc60e6c2a059299bed72ab44db054e826c60381
GodricLee <231502004@smail.nju.edu.cn>
2025-03-22 13:52:59 +0800

    CAAonLLM initialize

--- README.md
+++ README.md
@@ -1,19 +1,4 @@
-## **GPT2-Pytorch with Text-Generator**
-
-<p align="center"><img width="100" src="https://media-thumbs.golden.com/OLqzmrmwAzY1P7Sl29k2T9WjJdM=/200x200/smart/golden-storage-production.s3.amazonaws.com/topic_images/e08914afa10a4179893eeb07cb5e4713.png" /></p>
-
-**Better Language Models and Their Implications**
-
-> Our model, called GPT-2 (a successor to [GPT](https://blog.openai.com/language-unsupervised/)), was trained simply to predict the next word in 40GB of Internet text. Due to our concerns about malicious applications of the technology, we are not releasing the trained model. As an experiment in responsible disclosure, we are instead releasing a much [smaller model](https://github.com/openai/gpt-2) for researchers to experiment with, as well as a [technical paper](https://d4mucfpksywv.cloudfront.net/better-language-models/language_models_are_unsupervised_multitask_learners.pdf). from [openAI Blog](https://blog.openai.com/better-language-models/)
-
-This repository is simple implementation GPT-2 about **text-generator** in **Pytorch** with **compress code**
-
-- The original repertoire is [openai/gpt-2](https://github.com/openai/gpt-2). Also You can Read Paper about gpt-2, ["Language Models are Unsupervised Multitask Learners"](https://d4mucfpksywv.cloudfront.net/better-language-models/language-models.pdf). To Understand more detail concept, I recommend papers about Transformer Model.
-- Good implementation GPT-2 in Pytorch which I referred to, [huggingface/pytorch-pretrained-BERT](https://github.com/huggingface/pytorch-pretrained-BERT), You can see more detail implementation in huggingface repository.
-
-- Transformer(Self-Attention) Paper : [Attention Is All You Need(2017)](https://arxiv.org/abs/1706.03762)
-- First OpenAi-GPT Paper : [Improving Language Understanding by Generative Pre-Training(2018)](https://s3-us-west-2.amazonaws.com/openai-assets/research-covers/language-unsupervised/language_understanding_paper.pdf)
-- See [OpenAI Blog](https://blog.openai.com/better-language-models/) about GPT-2 and Paper
+## **CAA on LLM: GPT2-Pytorch**
 
 
 
@@ -21,7 +6,7 @@ This repository is simple implementation GPT-2 about **text-generator** in **Pyt
 
 1. download GPT2 pre-trained model in Pytorch which huggingface/pytorch-pretrained-BERT already made! (Thanks for sharing! it's help my problem transferring tensorflow(ckpt) file to Pytorch Model!)
 ```shell
-$ git clone https://github.com/graykode/gpt-2-Pytorch && cd gpt-2-Pytorch
+$ git clone https://github.com/GodricLee/CAAonLLM && cd CAAonLLM
 # download huggingface's pytorch model 
 $ curl --output gpt2-pytorch_model.bin https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-pytorch_model.bin
 # setup requirements, if using mac os, then run additional setup as descibed below
@@ -60,22 +45,11 @@ See more detail option about `temperature` and `top_k` in [here](https://github.
 
 - Pytorch 0.41+
 - regex 2017.4.5
-
-### Mac OS Setup
-```shell
-$ python3 -m venv venv
-$ source venv/bin/activate
-$ pip install torch tqdm
-$ brew install libomp
-$ export LC_ALL=en_US.UTF-8
-$ export LANG=en_US.UTF-8
-$ pip install -r requirements.txt
-```
+- 
 
 ## Author
 
-- Tae Hwan Jung(Jeff Jung) @graykode
-- Author Email : [nlkey2022@gmail.com](mailto:nlkey2022@gmail.com)
+- GodricLee
 
 
 
@@ -88,4 +62,5 @@ $ pip install -r requirements.txt
 
 ## Acknowledgement
 
-[Jeff Wu(@WuTheFWasThat)](https://github.com/WuTheFWasThat), [Thomas Wolf(@thomwolf)](https://github.com/thomwolf) for allowing referring code.
+- Tae Hwan Jung(Jeff Jung) @graykode
+- Email : [nlkey2022@gmail.com](mailto:nlkey2022@gmail.com)
--- main.py
+++ main.py
@@ -14,18 +14,11 @@ from GPT2.utils import load_weight
 from GPT2.config import GPT2Config
 from GPT2.sample import sample_sequence
 from GPT2.encoder import get_encoder
+import torch.optim as optim
 
-def text_generator(state_dict):
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--text", type=str, required=True)
-    parser.add_argument("--quiet", type=bool, default=False)
-    parser.add_argument("--nsamples", type=int, default=1)
-    parser.add_argument('--unconditional', action='store_true', help='If true, unconditional generation.')
-    parser.add_argument("--batch_size", type=int, default=-1)
-    parser.add_argument("--length", type=int, default=-1)
-    parser.add_argument("--temperature", type=float, default=0.7)
-    parser.add_argument("--top_k", type=int, default=40)
-    args = parser.parse_args()
+
+def text_generator(state_dict,args):
+    
 
     if args.quiet is False:
         print(args)
@@ -73,10 +66,149 @@ def text_generator(state_dict):
                 print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
             print(text)
 
-if __name__ == '__main__':
+
+
+
+
+
+def train(model, file_path, train_data, epochs=100, batch_size=8, lr=2e-5):
+    model.train()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    loss_fn = torch.nn.CrossEntropyLoss(ignore_index=-1)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    for epoch in range(epochs):
+        train_data = load_data(file_path, batch_size=batch_size)
+        for step, batch in enumerate(train_data):
+            input_ids, labels = batch
+            # 对输入和标签做右移处理，以实现自回归预测
+
+            # 调小学习率以避免小数据集上训练不稳定
+
+            # 冻结底层，仅训练高层，减少过拟合风险
+            for name, param in model.named_parameters():
+                if 'transformer.h.9' not in name:  # 只解冻最后一层
+                    param.requires_grad = False
+                # else:
+                    # print(f"Unfreezing {name} for training.")
+            # 移除序列的最后一个token作为输入，移除第一个token作为标签。
+            shifted_input_ids = input_ids[:, :-1].contiguous()
+            shifted_labels = labels[:, 1:].contiguous()
+
+            shifted_input_ids = shifted_input_ids.to(device)
+            shifted_labels    = shifted_labels.to(device)
+
+            optimizer.zero_grad()
+            outputs = model(shifted_input_ids, lm_labels=shifted_labels)
+            loss = outputs
+            loss.backward()
+            optimizer.step()
+
+            if step % 10 == 0:
+                print(f"Epoch {epoch}, Step {step}, Loss: {loss.item()}")
+
+# 示例数据加载函数
+def load_data(file_path, batch_size=8):
+    """
+    每行一条文本，积累到 batch_size 行后一次性返回。
+    """
+    encoder = get_encoder()
+    batch_input_ids = []
+    batch_labels = []
+    with open(file_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            text = line.strip()
+            if not text:
+                continue
+            input_ids = encoder.encode(text)
+            labels = input_ids
+            batch_input_ids.append(input_ids)
+            batch_labels.append(labels)
+
+            # 如果达到了一个批量，就 yield
+            if len(batch_input_ids) == batch_size:
+                yield _build_batch_tensors(batch_input_ids, batch_labels)
+                batch_input_ids, batch_labels = [], []
+
+    # 如果文件结束还有剩余数据，最后再返回一次
+    if batch_input_ids:
+        yield _build_batch_tensors(batch_input_ids, batch_labels)
+
+def _build_batch_tensors(batch_input_ids, batch_labels):
+    """
+    将列表中的数据转换为 (B, L) 维度的张量，这里 B 表示批大小 batch_size，L 表示序列长度。
+    """
+    max_len = max(len(ids) for ids in batch_input_ids)
+    # GPT-2 常用的 end of text token ID（如 50256）或 0 作为 input_ids 的填充值
+    # 保持 labels 的填充值为 -1
+    pad_id_for_input = 50256  
+    padded_input_ids = []
+    padded_labels = []
+    for ids, lbl in zip(batch_input_ids, batch_labels):
+        pad_len = max_len - len(ids)
+        # input_ids 用 GPT-2 索引范围内的 token（比如 50256）
+        padded_ids = ids + [pad_id_for_input] * pad_len
+        # labels 用 -1 便于后续 ignore
+        padded_lbl = lbl + [-1] * pad_len
+        padded_input_ids.append(padded_ids)
+        padded_labels.append(padded_lbl)
+
+    input_ids_tensor = torch.tensor(padded_input_ids)
+    labels_tensor    = torch.tensor(padded_labels)
+    return (input_ids_tensor, labels_tensor)
+
+
+def trainprocess(file_path):
     if os.path.exists('gpt2-pytorch_model.bin'):
+        seed = random.randint(0, 2147483647)
+        np.random.seed(seed)
+        torch.random.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
         state_dict = torch.load('gpt2-pytorch_model.bin', map_location='cpu' if not torch.cuda.is_available() else None)
-        text_generator(state_dict)
+        model = GPT2LMHeadModel(GPT2Config())
+        model = load_weight(model, state_dict)
+        model.to(device)
+
+        # 加载训练数据
+        batch_size = 8
+        train_data = load_data(file_path, batch_size=batch_size)
+
+        # 训练模型
+        train(model, file_path, train_data)
+
+        # 保存微调后的模型
+        torch.save(model.state_dict(), 'gpt2-finetuned_model.bin')
     else:
         print('Please download gpt2-pytorch_model.bin')
         sys.exit()
+
+if __name__ == '__main__':
+    if os.path.exists('gpt2-pytorch_model.bin'):
+        if torch.cuda.is_available():
+            print('Congrats: GPU is available.')
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--train", type=str, default=False)
+        parser.add_argument("--text", type=str, default=False)
+        parser.add_argument("--quiet", type=bool, default=False)
+        parser.add_argument("--nsamples", type=int, default=1)
+        parser.add_argument('--unconditional', action='store_true', help='If true, unconditional generation.')
+        parser.add_argument("--batch_size", type=int, default=-1)
+        parser.add_argument("--length", type=int, default=-1)
+        parser.add_argument("--temperature", type=float, default=0.7)
+        parser.add_argument("--top_k", type=int, default=20)
+        args = parser.parse_args()
+        file_path = 'train_data.txt'  # 训练数据文件路径
+        if args.train:
+            print('Training...')
+            trainprocess(file_path)
+            sys.exit()
+        else:
+            state_dict = torch.load('gpt2-finetuned_model.bin', map_location='cpu' if not torch.cuda.is_available() else None)
+            text_generator(state_dict,args)
+    else:
+        print('Please download gpt2-pytorch_model.bin')
+        sys.exit()
+
+
+
